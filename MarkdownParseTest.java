2d7097ea0cf23d1150f0bcaaefb7e78a913b43e2
Sanjith Devineni <sdevineni@ucsd.edu>
2022-04-29 11:12:30 -0700

    failure for test-file5
    JUnit version 4.13.2
    ......E
    Time: 0.009
    There was 1 failure:
    1) testfile5(MarkdownParseTest)
    java.lang.AssertionError: expected:<[]> but was:<[page.com]>
            at org.junit.Assert.fail(Assert.java:89)
            at org.junit.Assert.failNotEquals(Assert.java:835)
            at org.junit.Assert.assertEquals(Assert.java:120)
            at org.junit.Assert.assertEquals(Assert.java:146)
            at MarkdownParseTest.testfile5(MarkdownParseTest.java:66)
    
    FAILURES!!!
    Tests run: 6,  Failures: 1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -56,4 +56,13 @@ public class MarkdownParseTest { // creates a new class
         ArrayList<String> links = MarkdownParse.getLinks(content);
 	    assertEquals(expectedLinks, links);
     }
+
+    @Test
+    public void testfile5() throws IOException { 
+        ArrayList<String> expectedLinks = new ArrayList<String>();
+        Path fileName = Path.of("test-file5.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+	    assertEquals(expectedLinks, links);
+    }
 }
